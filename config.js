d07e060a922c054686158e80db808f7b70b2c21b
Kevin Kandlbinder <kevin@kevink.dev>
2021-04-04 16:12:52 +0200

    Standardize JavaScript Styling

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 module.exports = {
-    // The following is the default language the redirector will redirect to when no other language is found.
-    "default_language": "en",
-    // The following is a list of supported languages.
-    "supported_languages": ["de", "en"],
-    // The following is an array of paths the redirector should handle request for.
-    // Those can be literal paths (/foo/bar), paths with placeholders (/foo/*.bar) or paths with wildcards (/foo/**)
-    "listen_on_paths": [
-        "/",
-        "/legal/about"
-    ],
-    // This sets the worker to listen on all paths not prefixed with a language.
-    "listen_on_all_paths": false,
-    // DANGEROUS: The following sets the worker to run when a path already starts with /[some language]/something. This can lead to infinite redirects. Use responsibly.
-    "listen_on_prefixed_paths": false
-};
+  // The following is the default language the redirector will redirect to when no other language is found.
+  default_language: 'en',
+  // The following is a list of supported languages.
+  supported_languages: ['de', 'en'],
+  // The following is an array of paths the redirector should handle request for.
+  // Those can be literal paths (/foo/bar), paths with placeholders (/foo/*.bar) or paths with wildcards (/foo/**)
+  listen_on_paths: ['/', '/legal/about'],
+  // This sets the worker to listen on all paths not prefixed with a language.
+  listen_on_all_paths: false,
+  // DANGEROUS: The following sets the worker to run when a path already starts with /[some language]/something. This can lead to infinite redirects. Use responsibly.
+  listen_on_prefixed_paths: false
+}
--- index.js
+++ index.js
@@ -1,107 +1,108 @@
-import { matchPattern } from "url-matcher";
-import { pick } from "accept-language-parser";
+import { matchPattern } from 'url-matcher'
+import { pick } from 'accept-language-parser'
 
-import config from "./config.js";
-
+import config from './config.js'
 
 addEventListener('fetch', event => {
-  let request = event.request;
+  const request = event.request
 
   // Only run on GET or HEAD requests
-  if(request.method !== "GET" && request.method !== "HEAD") {
-    console.log("Request is non-GET, ignoring.");
-    return;
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    console.log('Request is non-GET, ignoring.')
+    return
   }
 
-  let url = new URL(request.url);
+  const url = new URL(request.url)
 
   // Check if the path is already prefixed when enabled in config.
-  if(!config.listen_on_prefixed_paths) {
-    let urlArray = url.pathname.split("/");
-    
+  if (!config.listen_on_prefixed_paths) {
+    const urlArray = url.pathname.split('/')
+
     // Ignore if the url has no first part e.g. /
-    if(urlArray.length > 2) {
-      let firstPart = urlArray[1];
+    if (urlArray.length > 2) {
+      const firstPart = urlArray[1]
 
-      let hasLanguage = false;
+      let hasLanguage = false
 
-      for(let i in config.supported_languages) {
-        let language = config.supported_languages[i];
+      for (const i in config.supported_languages) {
+        const language = config.supported_languages[i]
 
         // If there was a match, break from the loop.
-        if(language.toLowerCase() === firstPart.toLowerCase()) {
-          hasLanguage = true; 
-          break;
+        if (language.toLowerCase() === firstPart.toLowerCase()) {
+          hasLanguage = true
+          break
         }
       }
 
       // Return if a language was found.
-      if(hasLanguage) {
-        console.log("The request already has a language prefix, ignoring.");
-        return;
+      if (hasLanguage) {
+        console.log('The request already has a language prefix, ignoring.')
+        return
       }
     }
   }
 
   // Weather or not this request should be handled.
-  let handleThis = config.listen_on_all_paths;
+  let handleThis = config.listen_on_all_paths
 
   // Don't use route-matching when listen_on_all_paths is enabled.
-  if(!config.listen_on_all_paths) {
-    for(let i in config.listen_on_paths) {
-      let path = config.listen_on_paths[i];
-  
-      let match = matchPattern(path, url.pathname);
-  
-      console.log("Matching ", path, " against ", url.pathname, match);
-  
+  if (!config.listen_on_all_paths) {
+    for (const i in config.listen_on_paths) {
+      const path = config.listen_on_paths[i]
+
+      const match = matchPattern(path, url.pathname)
+
+      console.log('Matching ', path, ' against ', url.pathname, match)
+
       // If there was a match, break from the loop.
-      if(match && match.remainingPathname === "") {
-        handleThis = true;
-        break;
+      if (match && match.remainingPathname === '') {
+        handleThis = true
+        break
       }
     }
   }
 
   // Return if we are not supposed to handle this.
-  if(!handleThis) {
-    console.log("Request is not for us, ignoring.");
-    return;
+  if (!handleThis) {
+    console.log('Request is not for us, ignoring.')
+    return
   }
 
-  let headers = request.headers;
+  const headers = request.headers
 
   // Use default language if no Accept-Language Header was sent by the client.
-  if(!headers.has("Accept-Language")) {
-    event.respondWith(redirectWithPrefix(url, config.default_language));
-    return;
+  if (!headers.has('Accept-Language')) {
+    event.respondWith(redirectWithPrefix(url, config.default_language))
+    return
   }
 
-  let language = pick(config.supported_languages, headers.get("Accept-Language"));
+  let language = pick(
+    config.supported_languages,
+    headers.get('Accept-Language')
+  )
 
   // If accept-language-parser didn't manage to find a supported language, use the default one.
-  if(!language) {
-    language = config.default_language;
+  if (!language) {
+    language = config.default_language
   }
 
   // Do the redirect.
   event.respondWith(redirectWithPrefix(url, language))
 })
 
-
 /**
  * Generate a HTTP response redirecting to a URL, prefixed with a prefix.
  * @param {URL} url The URL the redirect is based on
- * @param {string} prefix The string to prefix "url" with.
+ * @param {string} prefix The string to prefix 'url' with.
  * @returns {Response} The final response
  */
-async function redirectWithPrefix(url, prefix) {
-  url.pathname = "/" + prefix + url.pathname;
+async function redirectWithPrefix (url, prefix) {
+  url.pathname = '/' + prefix + url.pathname
 
   return new Response(null, {
-    "status": 302,
-    "headers": new Headers({
-      "Location": url.href
+    status: 302,
+    headers: new Headers({
+      Location: url.href
     })
-  });
+  })
 }
